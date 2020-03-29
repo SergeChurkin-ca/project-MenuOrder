7210baf3dd8525986370c68b2ddd47c3ff1a669e
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-03-29 08:49:14 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 const flipCardBack = document.querySelector('.flip-card-back');
 const flipCardFront = document.querySelector('.flip-card-inner');
 const resetButton = document.getElementById('reset');
+const deliveryButton = document.getElementById('delivery');
 
 
-// check is checkbox is checked and totals 
+
+// check is checkbox is checked and totals
 function orderTotal() {
     var input = document.getElementsByName("product");
 
     var total = 0;
 
     for (var i = 0; i < input.length; i++) {
+
         if (input[i].checked) {
             input[i].checked = true;
             total += parseFloat(input[i].value);
@@ -17,10 +20,19 @@ function orderTotal() {
             flipCardFront.style = 'transition: none; transform: none'
         }
     }
-    document.getElementsByName("total")[0].value = "$" + total.toFixed(2);
 
-    // button has to be fixed : uncheck checkboxes and update total - this is temporary fix
-    resetButton.addEventListener('click', () => {
-        window.location.reload();
+    deliveryButton.addEventListener('click', () => {
+        document.getElementsByName("delivery")[0].value = "$" + 5;
     })
+
+    document.getElementsByName("total")[0].value = "$" + total.toFixed(2);
+    document.getElementsByName("taxes")[0].value = "$" + total.toFixed() * 0.13;
+    document.getElementsByName("grandtotal")[0].value = "$" + total * 1.13;
+
+
 }
+
+// button has to be fixed : uncheck checkboxes and update total - this is temporary fix
+resetButton.addEventListener('click', () => {
+    window.location.reload();
+})
